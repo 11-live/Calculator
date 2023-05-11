b1471046c4b0741f2d7fa9277b44521ece76557c
11-live <1987235901@qq.com>
2023-05-11 08:53:20 +0800

    删除多余

---
+++ 11-live/live-11/src/MainTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void solve() {
+
+        String sum = Main.Solve("11+22");
+        Assert.assertEquals("11+22=33", sum);
+    }
+}
